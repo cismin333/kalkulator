7380252b808282fdb903e7dfe8a8ce8ecfae6696
cismin333 <32226141+cismin333@users.noreply.github.com>
2018-04-04 17:40:36 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = 'htzpJLyLiNExpCCuusM6p79w/GTr2bRMI1B/T+2J4oegYBwu6A/M4TuBgSAymhDfxUUOGZyKg2WYzQKFAZW5movSNRq1hdXMBOxi26SSUlpC6amZCKqAoVWNVXNrssIZC0IsvZ9Gd43Qe0m1hh045QdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = '372542c0bc529dbaa59b8d436a62d72e';
